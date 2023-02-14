852a8a840456c394ef9f7f33af928e2ce6975adb
vince <vincent.harpin@orange.fr>
2023-02-14 13:00:52 +0100

    exercice 193

--- index.php
+++ index.php
@@ -14,64 +14,39 @@
 $server = 'localhost';
 $user = 'root';
 $pwd = '';
-$db = 'bdd_cours';
+$db = 'base_exercice';
 
-function sanitize($data) {
-    $data = trim($data);
-    $data = stripslashes($data);
-    $data = htmlspecialchars($data);
-    $data = addslashes($data);
-    return $data;
-}
+
 
 try {
 
     $maConnexion = new PDO("mysql:host=$server;dbname=$db;charset=utf8", $user, $pwd);
-    $maConnexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $maConnexion->beginTransaction();
 
     $user = "DELETE FROM user WHERE id = 2";
 
-    $result = $maConnexion->exec($user);
-    echo $result;
+    $maConnexion->exec($user);
 
     $user = "TRUNCATE TABLE user";
 
-    $result = $maConnexion->exec($user);
-    echo $result;
+    $maConnexion->exec($user);
 
-    $nom = sanitize('Scotte');
-    $prenom = sanitize('Vimme');
-    $rue = sanitize('Rue du rubban');
-    $numero = sanitize('7');
-    $code_postal = sanitize('59610');
-    $ville = sanitize('Fourmies');
-    $pays = sanitize('France');
-    $mail = sanitize('s.vimme@gmail.com');
+    $user = "
+         INSERT INTO user (nom, prenom, rue, numero, code_postal, ville, pays, mail)
+         VALUES ('Scotte', 'Vimme', 'Rue du rubban', '7', '59610', 'Fourmies', 'France', 's.vimme@gmail.com')
+    ";
 
-
-    $user = $maConnexion->prepare("
-         INSERT INTO utilisateur (nom, prenom, rue, numero, code_postal, ville, pays, mail)
-         VALUES (:Scotte, :Vimme, :Rue du rubban, :7, :59610, :Fourmies, :France, :s.vimme@gmail.com)
-    ");
-
-    $user->bindParam(':Scotte', $nom);
-    $user->bindParam(':Vimme', $prenom);
-    $user->bindParam(':Rue du rubban', $rue);
-    $user->bindParam(':7', $numero);
-    $user->bindParam(':59610', $code_postal,PDO::PARAM_INT);
-    $user->bindParam(':Fourmies', $vlle);
-    $user->bindParam(':France', $pays);
-    $user->bindParam(':s.vimme@gmail.com', $mail);
+    $maConnexion->exec($user);
 
     $user = "DROP TABLE user";
 
-    $result = $maConnexion->exec($user);
-    echo $result;
+    $maConnexion->exec($user);
 
     $user = "DROP DATABASE $db";
 
-    $result = $maConnexion->exec($user);
-    echo $result;
+    $maConnexion->exec($user);
+
+    $maConnexion->commit();
 }
 catch (PDOException $exception) {
     echo "Erreur de connexion: " . $exception->getMessage();
